a34d7b697f291e9e4136a66bde50551c1b5ba16e
cmajerhua <carlos.majerhua@unmsm.edu.pe>
2019-10-27 14:25:40 -0500

    subiendo 2 parte

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let periodoId = '';
 let $btnGuardarGasto = $("#guardar-gasto");
 let $precio = $("#precio");
 let $descripcion = $("#descripcion");
-
+let keyPeriodo;
 
 var jsFiltrar = document.querySelectorAll('.filtrar');
 var jsBuscar = document.querySelectorAll('.buscar');
@@ -16,50 +16,65 @@ var jsBuscar = document.querySelectorAll('.buscar');
         }).show();
     });
 
-$btnGuardarGasto.on('click',function(){
-    database.ref('Periodo/')
-    .orderByChild('vigente')
-    .equalTo(1)
-    .on('child_added',function(snapshot){
-        var periodo = snapshot.val();
-        if(periodo.vigente == 1){
-            periodoId = periodo.Id;
-            database.ref('Movimientos/').push({
-                descripcion: $descripcion.val(),
-                precio: $precio.val(),
-                fecha : String(moment(new Date()).format("DD/MM/YYYY HH:mm")),
-                periodoId: periodoId
-            });
-        }
+function guardar(){
+    database.ref('Movimientos/').push({
+        descripcion: $descripcion.val(),
+        precio: $precio.val(),
+        fecha : String(moment(new Date()).format("DD/MM/YYYY")),
+        hora: String(moment(new Date()).format("HH:mm")),
+        periodoId: periodoId
     });
-    console.log("Guardando");
-});
+}
 
 listarMovimientos();
 let tableBody = document.querySelector("#table-responsive-body");
 
 function listarMovimientos(){
-    
-    database.ref('Movimientos').on('value',function(snap){
-        let html='';
-        let arrayMov = Object.values(snap.val());
-        console.log("tam array =>",arrayMov)
-        for(let i=0;i< arrayMov.length ; i++){
-            html+= `<tr>`;
-            html+= `<td>${arrayMov[i].descripcion}</td>`;
-            html+= `<td>S/.${arrayMov[i].precio}</td>`;
-            html+= `<td>${arrayMov[i].fecha}</td>`;
-            html+= `<td><button  type='button' class='btn btn-danger btn-sm'>Eliminar</button></td>`;
-            html+= `</tr>`;
+    database.ref('Periodo/')
+    .orderByChild('vigente')
+    .equalTo(1)
+    .on('child_added',function(snapshot){
+        console.log("SnapShot =>",snapshot);
+        keyPeriodo = snapshot.key;
+        var periodo = snapshot.val();
+        periodoId = periodo.id;
+        console.log("Periodo =>",periodo.id);
+        if(periodo.vigente == 1){
+            
+            database.ref('Movimientos').on('value',function(snap){
+                let html='';
+                    $.each(snap.val(),function(index,value){
+                        if(value.periodoId==periodoId){
+                            html+= `<tr>`;
+                            html+= `<td>${value.descripcion}</td>`;
+                            html+= `<td>S/.${value.precio}</td>`;
+                            html+= `<td>${value.fecha}</td>`;
+                            html+= `<td><button  type='button' class='btn btn-danger btn-sm' onclick='eliminar("${index}")' >Eliminar</button></td>`;
+                            html+= `</tr>`;
+                        }
+                    })
+                tableBody.innerHTML = html;
+            });
         }
-        tableBody.innerHTML = html;
-        // console.log(Object.values(snap.val()));
     });
 }
 
-console.log("uuid =>",uuid.v1());
-// database.ref('Periodo/').push({
-//     id: "2",
-//     fecha : String(moment(new Date()).format("DD/MM/YYYY")),
-//     vigente: 0
-//   });
+function eliminar(id){
+    var refId = database.ref('Movimientos').child(id);
+    refId.remove();
+}
+
+function nuevoPeriodo(){
+
+    var refPeriodoId = database.ref('Periodo').child(keyPeriodo)
+    refPeriodoId.update({
+        vigente: 0
+    }).then(function(res){
+        console.log("Nuevo Periodo");
+        database.ref('Periodo/').push({
+            id: uuid.v1(),
+            fecha : String(moment(new Date()).format("DD/MM/YYYY")),
+            vigente: 1
+        })
+    });
+}
