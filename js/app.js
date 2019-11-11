4878a740d9b67cc2da83e3554bd7fb944202a73e
cmajerhua <carlos.majerhua@unmsm.edu.pe>
2019-11-10 22:47:45 -0500

    agregando estilos

--- js/app.js
+++ js/app.js
@@ -1,20 +1,30 @@
 var database = firebase.database();
 let periodoId = '';
+let tableBody = document.querySelector("#table-responsive-body");
 let $btnGuardarGasto = $("#guardar-gasto");
 let $precio = $("#precio");
 let $descripcion = $("#descripcion");
+let $total = $("#total");
+let $mElimId = $("#m-elim-id");
 let keyPeriodo;
 
-var jsFiltrar = document.querySelectorAll('.filtrar');
-var jsBuscar = document.querySelectorAll('.buscar');
-    $(jsFiltrar).keyup(function () {
-        var idfil = $(this).attr('ident-buscar');
-        var rex = new RegExp($(this).val(),'i');
-        $(jsBuscar).children('tr').hide();
-        $(jsBuscar).children('tr').filter(function () {
-            return rex.test($(this).text());
-        }).show();
-    });
+
+$(document).ready(function(){
+
+    var jsFiltrar = document.querySelectorAll('.filtrar');
+    var jsBuscar = document.querySelectorAll('.buscar');
+        $(jsFiltrar).keyup(function () {
+            var idfil = $(this).attr('ident-buscar');
+            var rex = new RegExp($(this).val(),'i');
+            $(jsBuscar).children('tr').hide();
+            $(jsBuscar).children('tr').filter(function () {
+                return rex.test($(this).text());
+            }).show();
+        });
+    
+    listarMovimientos();
+    $('[data-toggle="tooltip"]').tooltip();   
+});
 
 function guardar(){
     database.ref('Movimientos/').push({
@@ -25,11 +35,13 @@ function guardar(){
         periodoId: periodoId
     }).then(function(res){
         $("#myModal").modal('toggle');
+        $precio.val("");
+        $descripcion.val("");
     });
 }
 
-listarMovimientos();
-let tableBody = document.querySelector("#table-responsive-body");
+
+
 
 function listarMovimientos(){
     database.ref('Periodo/')
@@ -52,10 +64,9 @@ function listarMovimientos(){
                             html+= `<td>${value.descripcion}</td>`;
                             html+= `<td>S/.${value.precio}</td>`;
                             html+= `<td>${value.fecha}</td>`;
-                            html+= `<td><button  type='button' class='btn btn-danger btn-sm' onclick='eliminar("${index}")' ><i class="far fa-trash-alt"></i></button></td>`;
+                            html+= `<td><button  type='button' onclick='setIdReq("${index}")' class='btn btn-danger btn-sm' data-toggle="modal" data-target="#modal-eliminar" ><i class="far fa-trash-alt"></i></button></td>`;
                             html+= `</tr>`;
-                            total+= parseFloat(value.precio);
-                            
+                            total+= parseFloat(value.precio);                         
                         }
                     });
                 $("#total").text("S/."+total);
@@ -65,9 +76,32 @@ function listarMovimientos(){
     });
 }
 
-function eliminar(id){
-    var refId = database.ref('Movimientos').child(id);
-    refId.remove();
+$precio.on('keyup',function(e){
+
+    let val = String($precio.val());
+
+    let re = /([.])\1/;
+    //if(re.test(val))
+        //$precio.val(val.substring(0,val.length-1));
+    console.log(re.test(val));
+    if( ( val.charCodeAt(val.length-1) >=48 && val.charCodeAt(val.length-1)<=57) || val.charCodeAt(val.length-1)==46){
+
+
+
+    }else{
+        $precio.val(val.substring(0,val.length-1));
+    }
+});
+
+function eliminar(){
+    var refId = database.ref('Movimientos').child($mElimId.val());
+    refId.remove().then(function(res){
+        $("#modal-eliminar").modal('toggle');
+    });
+}
+
+function setIdReq(id){
+    $mElimId.val(id);
 }
 
 function nuevoPeriodo(){
@@ -83,4 +117,8 @@ function nuevoPeriodo(){
             vigente: 1
         })
     });
+}
+
+function openModalNewGasto(){
+    $("#modal-guardar").modal('toggle');
 }
